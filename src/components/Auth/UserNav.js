d9385a978fd1e16f82559c1c440029fbc10afcb8
caiomarqs <caiomarques1412@gmail.com>
2022-10-02 14:51:37 -0300

    Add loug ouy on nav bar

--- src/components/Auth/UserNav.js
+++ src/components/Auth/UserNav.js
@@ -1,10 +1,52 @@
-import { AppBar } from '@mui/material'
 import React from 'react'
 
-const UserNav = () => {
-    return (
-        <AppBar position='static'>
+import { AppBar, Typography } from '@mui/material'
+import { useSession, signOut } from 'next-auth/react'
 
+import { RomaLogo } from '../Icons'
+import { ContainedBaseButton } from '../Buttons'
+
+const UserNav = () => {
+
+    const session = useSession()
+    const { data } = session
+
+    return (
+        <AppBar
+            position='static'
+        >
+            <div style={{
+                padding: '0.19rem 0 0 0',
+                marginRight: '1.5rem'
+            }}>
+                <RomaLogo height='24' />
+
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                    alignItems: 'center'
+                }}
+            >
+                <Typography
+                    variant='subtitle2'
+                    component="span"
+                    sx={{ fontWeight: 'bold' }}
+                >
+                    Olá, {data?.user?.name}
+                </Typography>
+                <ContainedBaseButton
+                    sx={{
+                        margin: '0 0 0 1.5rem'
+                    }}
+                    onClick={() => signOut()}
+                >
+                    Sair
+                </ContainedBaseButton>
+            </div>
         </AppBar>
     )
 }
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,5 +1,5 @@
 import { createTheme } from '@mui/material/styles';
-import { borderRadius } from '@mui/system';
+import { gray } from './colors';
 import { fontsSize } from './fonts';
 
 import {
@@ -28,11 +28,28 @@ const theme = createTheme({
           fontSize: fontsSize.inputFonts,
           textTransform: 'capitalize',
           borderRadius: '.5rem',
-          minWidth: '128px'
+          minWidth: '128px',
+          fontWeight: 'bold'
         }
       },
       defaultProps:{
         size: 'small'
+      }
+    },
+    MuiAppBar: {
+      styleOverrides:{
+        root:{
+          backgroundColor: '#fff',
+          boxShadow: 'none',
+          height: '3.75rem',
+          padding: '0 1.5rem',
+          borderBottom: `0.0625rem solid ${gray.A500}`,
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          color: gray.A200,
+        },
+        colorPrimary: gray.A200,
       }
     }
   },
